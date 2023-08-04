dbed9f6799f0c891fb98511d123ef91b615d1698
A-d-a-m <adam3928@mail.ru>
2023-08-04 10:45:19 +0200

    initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope='function', autouse=True)
+def driver_management():
+    browser.config.base_url = 'https://todomvc.com/examples/emberjs'
+    driver_options = browser.config.driver_options = webdriver.FirefoxOptions()
+    driver_options.add_argument('--headless')
+    browser.config.driver_options = driver_options
+
+    yield
+
+    browser.quit()
