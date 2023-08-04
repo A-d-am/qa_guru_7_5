8b1113947e991e18ea132458e3fd3e2a7436dd82
A-d-a-m <adam3928@mail.ru>
2023-08-04 15:51:38 +0200

    initial commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def driver_management():
-    browser.config.base_url = 'https://todomvc.com/examples/emberjs'
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     driver_options = browser.config.driver_options = webdriver.FirefoxOptions()
     driver_options.add_argument('--headless')
     browser.config.driver_options = driver_options
---
+++ tests/test_demoqa_form.py
@@ -0,0 +1,5 @@
+from selene import browser, have, be
+
+
+def test_fill_and_send_form():
+    browser.open('/')
