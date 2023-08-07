f30eab8842f386eb6922a58ff5b2abd1dcf38a66
A-d-a-m <adam3928@mail.ru>
2023-08-07 15:41:41 +0200

    deleted not used imports

--- tests/test_demoqa_form.py
+++ tests/test_demoqa_form.py
@@ -1,7 +1,6 @@
 import os
 
-from selene import browser, have, be, by, command
-
+from selene import browser, have, be
 
 def test_fill_and_send_form():
     browser.open('/automation-practice-form')
