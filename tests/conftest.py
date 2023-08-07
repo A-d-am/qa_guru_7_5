cc98019f49a66d3493fb0594ead12123d305285b
A-d-a-m <adam3928@mail.ru>
2023-08-07 15:40:07 +0200

    reworked base url and driver options

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,12 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def driver_management():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-    driver_options = browser.config.driver_options = webdriver.FirefoxOptions()
+    browser.config.base_url = 'https://demoqa.com'
+    driver_options = webdriver.ChromeOptions()
     driver_options.add_argument('--headless')
     browser.config.driver_options = driver_options
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield
 
